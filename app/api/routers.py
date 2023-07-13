4ccaac406cbaef54cd9167ef155d4acd07a8f552
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-13 22:28:22 +0300

    feature: charity project router added

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter
 from app.api.endpoints import (
     user_router,
+    charity_project_router
 )
 
 main_router = APIRouter()
 
 main_router.include_router(
     user_router,
+)
+
+main_router.include_router(
+    charity_project_router,
+    prefix='/charity_project',
+    tags=['Charity Projects']
 )
