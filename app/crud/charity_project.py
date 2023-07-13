aa87a116d98a867058b92fa56d3a632121db552c
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-13 22:30:10 +0300

    feature: method to get project id by name added to CRUDCharityProject

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,9 +1,27 @@
+from typing import Optional
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
 from app.models import CharityProject
 
 
 class CRUDCharityProject(CRUDBase):
-    pass
+    async def get_id_by_name(
+            self,
+            project_name: str,
+            session: AsyncSession,
+
+    ) -> Optional[int]:
+        db_project_id = await session.execute(
+            select(
+                self.model.id,
+            ).where(
+                self.model.name == project_name
+            )
+        )
+        db_project_id = db_project_id.scalars().first()
+        return db_project_id
 
 
 charity_project_crud = CRUDCharityProject(CharityProject)
