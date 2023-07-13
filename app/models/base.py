b1c43138311d5909927de7f47650af4463fd4f87
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-13 23:35:43 +0300

    feature: default value from create_date field added

--- app/models/base.py
+++ app/models/base.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import Column, Integer, DateTime
 
 from app.core.db import Base
@@ -7,5 +9,5 @@ class CharityBase(Base):
     __abstract__ = True
     full_amount = Column(Integer, default=0)
     invested_amount = Column(Integer, default=0)
-    create_date = Column(DateTime)
+    create_date = Column(DateTime, default=datetime.now())
     close_date = Column(DateTime)
