34503a2848685f9bd12ba4dcc0bed9186ee2865c
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-13 23:39:47 +0300

    feature: not empty description check added to CharityProjectUpdate scheme, CharityProjectCreate inherited from CharityProjectUpdate

--- app/schemas/charity_project.py
+++ app/schemas/charity_project.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import Field
+from pydantic import Field, validator
 
 from .base import CharityBase
 
@@ -17,21 +17,32 @@ class CharityProjectBase(CharityBase):
 
 class CharityProjectDB(CharityProjectBase):
     id: int
-
-    class Config:
-        orm_mode = True
-
-
-class CharityProjectCreate(CharityProjectBase):
+    fully_invested: bool
     name: str = Field(
         ...,
         min_length=1,
         max_length=100,
         title='Project name'
     )
-    description: str
-    full_amount: int = Field(..., gt=0)
+
+    class Config:
+        orm_mode = True
 
 
 class CharityProjectUpdate(CharityProjectBase):
-    pass
+    @validator('description')
+    def check_description_is_not_empty(cls, value: str):
+        if not value:
+            raise ValueError('Description cannot be empty')
+        return value
+
+
+class CharityProjectCreate(CharityProjectUpdate):
+    name: str = Field(
+        ...,
+        min_length=1,
+        max_length=100,
+        title='Project name'
+    )
+    description: str = Field()
+    full_amount: int = Field(..., ge=0)
