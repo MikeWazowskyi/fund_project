90bbbd14d060ba617a412e7cd5ca18bc047fb785
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-15 15:53:12 +0300

    feature: create method inherited from base class CRUDBase

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,12 +1,27 @@
+from datetime import datetime
 from typing import Optional
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
 from app.models import CharityProject
+from app.schemas.charity_project import CharityProjectCreate
 
 
 class CRUDCharityProject(CRUDBase):
+    async def create(
+            self,
+            charity_project: CharityProjectCreate,
+            session: AsyncSession,
+    ):
+        project_data = charity_project.dict()
+        project_data['create_date'] = datetime.now()
+        charity_project = self.model(**project_data, )
+        session.add(charity_project)
+        await session.commit()
+        await session.refresh(charity_project)
+        return charity_project
+
     async def get_id_by_name(
             self,
             project_name: str,
