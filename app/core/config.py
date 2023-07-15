fd8b662f787dad8b58b5d8247062fbdb5b50c2a4
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-15 17:24:48 +0300

    feature: default value added for database_url attribute

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     app_title: str = 'Кошачий благотворительный фонд'
     description: str = 'API-сервис для кошачьего благотворительного фонда'
-    database_url: str
+    database_url: str = 'sqlite+aiosqlite:///./charity_fund.db'
     secret: str = 'SECRET_KEY'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
