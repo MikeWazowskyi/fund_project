90b0fc6f9e3a83f5c561ddcd8590b8c68dc7bd45
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-15 17:26:53 +0300

    feature: donation router added to main router

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from app.api.endpoints import (
     user_router,
-    charity_project_router
+    charity_project_router,
+    donation_router,
 )
 
 main_router = APIRouter()
@@ -14,4 +15,10 @@ main_router.include_router(
     charity_project_router,
     prefix='/charity_project',
     tags=['Charity Projects']
+)
+
+main_router.include_router(
+    donation_router,
+    prefix='/donation',
+    tags=['Donations']
 )
