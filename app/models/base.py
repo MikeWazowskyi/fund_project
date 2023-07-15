f6a73ae60916095ff069807282bbbf77d598cb48
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-15 17:23:14 +0300

    feature: fully_invested field created in base sqlalchemy model

--- app/models/base.py
+++ app/models/base.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy import Column, Integer, DateTime, Boolean
 
 from app.core.db import Base
 
@@ -8,6 +8,7 @@ from app.core.db import Base
 class CharityBase(Base):
     __abstract__ = True
     full_amount = Column(Integer, default=0)
+    fully_invested = Column(Boolean, default=False)
     invested_amount = Column(Integer, default=0)
     create_date = Column(DateTime, default=datetime.now())
     close_date = Column(DateTime)
