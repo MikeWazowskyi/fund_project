8bdd57dcca3e0051dfa5f2250d111c1bde4057c7
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-15 17:24:25 +0300

    feature: fully_invested field removed

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, String, Text, Boolean
+from sqlalchemy import Column, String, Text
 from .base import CharityBase
 
 
 class CharityProject(CharityBase):
     name = Column(String(100), unique=True)
     description = Column(Text)
-    fully_invested = Column(Boolean, default=False)
+
