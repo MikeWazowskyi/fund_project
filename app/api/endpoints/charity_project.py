245939cea85a3c198c297cd9428b3aa832a499b7
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-15 15:51:34 +0300

    feature: delete and patch endpoints added to charity project router

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -1,14 +1,15 @@
 from typing import List, Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.endpoints import validators
+from app.api.endpoints.validators import check_charity_project_before_edit
 from app.core.db import get_async_session
 from app.core.user import current_superuser
 from app.crud.charity_project import charity_project_crud
 from app.schemas.charity_project import CharityProjectDB, \
-    CharityProjectCreate
+    CharityProjectCreate, CharityProjectUpdate
 
 router = APIRouter()
 
@@ -42,3 +43,54 @@ async def get_all_charity_projects(
 ):
     charity_projects = await charity_project_crud.get_multi(session)
     return charity_projects
+
+
+@router.delete(
+    '/{charity_project_id}',
+    response_model=CharityProjectDB,
+    dependencies=[Depends(current_superuser)],
+)
+async def remove_charity_project(
+        charity_project_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    charity_project = await validators.check_charity_project_exists(
+        charity_project_id,
+        session
+    )
+    charity_project = await charity_project_crud.remove(
+        charity_project,
+        session
+    )
+    return charity_project
+
+
+@router.patch(
+    '/{charity_project_id}',
+    response_model=CharityProjectDB,
+    # dependencies=[Depends(current_superuser)],
+)
+async def partially_update_charity_project(
+        charity_project_id: int,
+        charity_project_data: CharityProjectUpdate,
+        session: AsyncSession = Depends(get_async_session),
+):
+    allowed_fields = {'name', 'description', 'full_amount'}
+    charity_project = await validators.check_charity_project_exists(
+        charity_project_id,
+        session,
+    )
+
+    await check_charity_project_before_edit(
+        charity_project_data,
+        charity_project,
+        session,
+        allowed_to_edit_fields=allowed_fields,
+    )
+
+    charity_project = await charity_project_crud.update(
+        charity_project,
+        charity_project_data,
+        session,
+    )
+    return charity_project
