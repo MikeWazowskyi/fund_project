dcdc1e6ae73ebd60f83ac9a2ad1f24e85b38c722
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-15 17:26:13 +0300

    feature: method to get model instances by related user_id added

--- app/crud/donation.py
+++ app/crud/donation.py
@@ -1,9 +1,27 @@
+from typing import List, Optional
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
-from app.models import Donation
+from app.models import Donation, User
+from app.schemas.donation import DonationDB, DonationCreate
 
 
 class CRUDDonation(CRUDBase):
-    pass
+
+    async def get_by_user(
+            self,
+            user_id: int,
+            session: AsyncSession,
+    ) -> List[DonationDB]:
+        donations = await session.execute(
+            select(self.model).where(
+                self.model.user_id == user_id
+            )
+        )
+        donations = donations.scalars().all()
+        return donations
 
 
 donation_crud = CRUDDonation(Donation)
