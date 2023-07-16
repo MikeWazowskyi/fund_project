030cae90d380f93cadd6193189a5c5eb5e110aed
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-16 19:42:14 +0300

    feature: docstrings added

--- app/models/base.py
+++ app/models/base.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, DateTime, Boolean
+from sqlalchemy import Boolean, Column, DateTime, Integer
 
 from app.core.db import Base
 
 
 class CharityBase(Base):
+    """Abstract sqlalchemy model"""
     __abstract__ = True
     full_amount = Column(Integer, default=0)
     fully_invested = Column(Boolean, default=False)
