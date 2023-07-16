3b6c6d3f9c8f304f313213931d8889dd3fe49ea3
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-16 19:40:17 +0300

    feature: docstrings added, __repr__ added

--- app/models/donation.py
+++ app/models/donation.py
@@ -1,8 +1,15 @@
-from sqlalchemy import ForeignKey, Integer, Column, Text, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, Text
 
 from app.models.base import CharityBase
 
 
 class Donation(CharityBase):
+    """Donation sqlalchemy model"""
+
     user_id = Column(Integer, ForeignKey('user.id'))
     comment = Column(Text)
+
+    def __repr__(self):
+        return (f'<user_id: {self.user_id}, '
+                f'created: {self.create_date},'
+                f'full_amount: {self.full_amount}>')
