25c6f9b7b6535dbf3b4264d459a187da68b7551c
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-16 19:34:02 +0300

    feature: docstrings added

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,12 +2,15 @@ from fastapi_users import schemas
 
 
 class UserRead(schemas.BaseUser[int]):
+    """Read from DB user pydantic schema"""
     pass
 
 
 class UserCreate(schemas.BaseUserCreate):
+    """Create user pydantic schema"""
     pass
 
 
 class UserUpdate(schemas.BaseUserUpdate):
+    """Update user pydantic schema"""
     pass
