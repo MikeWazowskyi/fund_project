055bf93ed32083953a733536af90a78add489e21
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-16 21:47:53 +0300

    bugfix: full amount minimum value set as greater then 0

--- app/schemas/base.py
+++ app/schemas/base.py
@@ -12,7 +12,7 @@ CLOSE_DATE = (datetime.now() + timedelta(days=30)).isoformat(
 class CharityBase(BaseModel):
     """Base pydantic schema"""
 
-    full_amount: Optional[int] = Field(None, ge=0)
+    full_amount: Optional[int] = Field(None, gt=0)
     fully_invested: Optional[bool]
     invested_amount: Optional[int] = Field(None, ge=0)
     create_date: Optional[datetime] = Field(None, example=CREATE_DATE)
