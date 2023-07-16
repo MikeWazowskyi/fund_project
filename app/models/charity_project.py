90b7aa11ca8654caa3e8a7435790f86b7695bcfd
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-16 14:30:35 +0300

    feature: __repr__ added

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -6,3 +6,7 @@ class CharityProject(CharityBase):
     name = Column(String(100), unique=True)
     description = Column(Text)
 
+    def __repr__(self):
+        return (f'<Name: {self.name}, '
+                f'created: {self.create_date},'
+                f'invested: {self.fully_invested}>')
