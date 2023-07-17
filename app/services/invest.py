be46ff8af824de2b2230b6d83e49173b624ffb7d
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-17 18:59:50 +0300

    feature: invest coroutine was rewritten as sync

--- app/services/invest.py
+++ app/services/invest.py
@@ -1,40 +1,28 @@
 from datetime import datetime
+from typing import List
 
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.crud.base import CRUDBase
 from app.models.base import CharityBase
 
 
-async def invest(
-        obj_to_invest: CharityBase,
-        crud_object: CRUDBase,
-        session: AsyncSession,
+def invest(
+        target: CharityBase,
+        sources: List[CharityBase],
 ):
     """Coroutine to perform investing logic"""
 
-    not_invested_objs = await crud_object.get_not_invested(
-        session
-    )
-    try:
-        for not_invested_obj in not_invested_objs:
-            left_to_invest = (not_invested_obj.full_amount -
-                              not_invested_obj.invested_amount)
-            free_to_invest = (obj_to_invest.full_amount -
-                              obj_to_invest.invested_amount)
-            amount_to_invest = min(free_to_invest, left_to_invest)
-            close_if_invested(not_invested_obj, amount_to_invest)
-            close_if_invested(obj_to_invest, amount_to_invest)
-
-    except Exception:
-        await session.rollback()
-    else:
-        await session.commit()
-        await session.refresh(obj_to_invest)
+    for source in sources:
+        left_to_invest = (source.full_amount -
+                          source.invested_amount)
+        free_to_invest = (target.full_amount -
+                          target.invested_amount)
+        amount_to_invest = min(free_to_invest, left_to_invest)
+        close_if_invested(source, amount_to_invest)
+        close_if_invested(target, amount_to_invest)
+    return target, sources
 
 
-def close_if_invested(model: CharityBase, amount_to_invest: int):
-    model.invested_amount += amount_to_invest
-    if model.full_amount == model.invested_amount:
-        model.fully_invested = True
-        model.close_date = datetime.now()
+def close_if_invested(obj: CharityBase, amount_to_invest: int):
+    obj.invested_amount += amount_to_invest
+    if obj.full_amount == obj.invested_amount:
+        obj.fully_invested = True
+        obj.close_date = datetime.now()
