196031880cbe32bf40ce392302cfe3657ccca308
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-17 23:47:14 +0300

    refactoring: close_if_invested function added to CRUDBase class as static method from app.services.invest

--- app/services/invest.py
+++ app/services/invest.py
@@ -1,6 +1,6 @@
-from datetime import datetime
 from typing import List
 
+from app.crud.base import CRUDBase
 from app.models.base import CharityBase
 
 
@@ -9,20 +9,10 @@ def invest(
         sources: List[CharityBase],
 ):
     """Coroutine to perform investing logic"""
-
     for source in sources:
-        left_to_invest = (source.full_amount -
-                          source.invested_amount)
-        free_to_invest = (target.full_amount -
-                          target.invested_amount)
+        left_to_invest = (source.full_amount - source.invested_amount)
+        free_to_invest = (target.full_amount - target.invested_amount)
         amount_to_invest = min(free_to_invest, left_to_invest)
-        close_if_invested(source, amount_to_invest)
-        close_if_invested(target, amount_to_invest)
+        CRUDBase.close_if_invested(source, amount_to_invest)
+        CRUDBase.close_if_invested(target, amount_to_invest)
     return target, sources
-
-
-def close_if_invested(obj: CharityBase, amount_to_invest: int):
-    obj.invested_amount += amount_to_invest
-    if obj.full_amount == obj.invested_amount:
-        obj.fully_invested = True
-        obj.close_date = datetime.now()
