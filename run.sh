b8c390a8e8843b168512e9bafa4f09a18419df8b
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-17 11:34:55 +0300

    feature: run.sh file added

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+python -m pip install --upgrade pip
+python -m venv venv
+source venv\bin\activate
+python -m pip install --no-cache-dir -r requirements.txt
+alembic upgrade head
+uvicorn app.main:app --host localhost --port 80
