f8b7d0b975efa3ffeb24c0136805b4326b7ffa27
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-07-18 16:57:07 +0300

    feature: database constraints added

--- app/models/base.py
+++ app/models/base.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime, Integer
+from sqlalchemy import Boolean, Column, DateTime, Integer, CheckConstraint
 
 from app.core.db import Base
 
@@ -8,6 +8,12 @@ from app.core.db import Base
 class CharityBase(Base):
     """Abstract sqlalchemy model"""
     __abstract__ = True
+    __table_args__ = (
+        CheckConstraint(
+            'full_amount > 0',
+            'full_amount >= invested_amount',
+        ),
+    )
     full_amount = Column(Integer, default=0)
     fully_invested = Column(Boolean, default=False)
     invested_amount = Column(Integer, default=0)
