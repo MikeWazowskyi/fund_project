d4576d27c6f3780adc84fee3c3df031a5bb8bb73
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-08-01 13:15:31 +0300

    feature: invest function changed: investing operations stops if amount_to_invest equals to zero; function returns only invested projects.

--- app/services/invest.py
+++ app/services/invest.py
@@ -1,6 +1,5 @@
 from typing import List
 
-from app.crud.base import CRUDBase
 from app.models.base import CharityBase
 
 
@@ -9,10 +8,14 @@ def invest(
         sources: List[CharityBase],
 ):
     """Coroutine to perform investing logic"""
+    invested = []
     for source in sources:
         left_to_invest = (source.full_amount - source.invested_amount)
         free_to_invest = (target.full_amount - target.invested_amount)
         amount_to_invest = min(free_to_invest, left_to_invest)
-        CRUDBase.close_if_invested(source, amount_to_invest)
-        CRUDBase.close_if_invested(target, amount_to_invest)
-    return target, sources
+        if amount_to_invest == 0:
+            break
+        for participant in sources, target:
+            participant.close_if_invested(amount_to_invest)
+        invested.append(source)
+    return target, invested
